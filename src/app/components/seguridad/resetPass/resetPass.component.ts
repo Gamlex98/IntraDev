e271afd1c290494c7bfbe2ee29570d3bec412d3f
Fredy Alexander FAOC. Obando Cordoba <aprendiz.desarrollo@fresmar.local>
2023-07-19 11:17:37 -0500

    Intranet Dev

---
+++ src/app/components/seguridad/resetPass/resetPass.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resetPass',
+  templateUrl: './resetPass.component.html',
+  styleUrls: ['./resetPass.component.css']
+})
+export class ResetPassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
